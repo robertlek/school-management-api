5a98e3e9c24c21d78e3fef6aed3dabccd9c55ee1
robertlek <lecarobertandrei@gmail.com>
2024-03-18 15:30:51 +0200

    Implemented an endpoint for creating a new student in the 'Student' controller.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using school_management_api.Models;
+using school_management_api.Models.DTOs;
+using school_management_api.Storage.Context;
+
+namespace school_management_api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class StudentController(DatabaseContext db) : ControllerBase
+{
+    private readonly DatabaseContext _db = db;
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> CreateStudent([FromBody] StudentCreateDTO studentDTO)
+    {
+        if (studentDTO == null)
+        {
+            return BadRequest();
+        }
+
+        StudentModel student = new()
+        {
+            Id = Guid.NewGuid(),
+            FirstName = studentDTO.FirstName,
+            LastName = studentDTO.LastName,
+            DateOfBirth = studentDTO.DateOfBirth,
+        };
+
+        await _db.Students.AddAsync(student);
+        await _db.SaveChangesAsync();
+
+        return NoContent();
+    }
+}
---
+++ Models/DTOs/StudentCreateDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace school_management_api.Models.DTOs;
+
+public class StudentCreateDTO
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public DateTime DateOfBirth { get; set; }
+}
