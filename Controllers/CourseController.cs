d53bb4186ec4746f714da6f540dfee13bc464013
robertlek <lecarobertandrei@gmail.com>
2024-03-19 14:21:22 +0200

    Implemented 'Course' controller with an endpoint used to create a new course.

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using school_management_api.Models.DTOs;
+using school_management_api.Models;
+using school_management_api.Storage.Context;
+
+namespace school_management_api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CourseController(DatabaseContext db) : ControllerBase
+{
+    private readonly DatabaseContext _db = db;
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> CreateCourse([FromBody] CourseCreateDTO courseDTO)
+    {
+        if (courseDTO == null)
+        {
+            return BadRequest();
+        }
+
+        CourseModel course = new()
+        {
+            Id = Guid.NewGuid(),
+            TeacherId = courseDTO.TeacherId,
+            Name = courseDTO.Name,
+            Description = courseDTO.Description,
+            AttendedStudents = 0
+        };
+
+        await _db.Courses.AddAsync(course);
+        await _db.SaveChangesAsync();
+
+        return NoContent();
+    }
+}
---
+++ Models/DTOs/CourseCreateDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace school_management_api.Models.DTOs;
+
+public class CourseCreateDTO
+{
+    public Guid TeacherId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+}
