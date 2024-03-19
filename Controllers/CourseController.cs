e6b41cafc1718290f653bae1a28e2a514136112d
robertlek <lecarobertandrei@gmail.com>
2024-03-19 14:58:36 +0200

    Implemented an endpoint to fetch all courses in the 'Course' API controller.

--- Controllers/CourseController.cs
+++ Controllers/CourseController.cs
@@ -2,6 +2,7 @@
 using school_management_api.Models.DTOs;
 using school_management_api.Models;
 using school_management_api.Storage.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace school_management_api.Controllers;
 
@@ -10,6 +11,13 @@ namespace school_management_api.Controllers;
 public class CourseController(DatabaseContext db) : ControllerBase
 {
     private readonly DatabaseContext _db = db;
+
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult<List<CourseModel>>> GetAllCourses()
+    {
+        return Ok(await _db.Courses.ToListAsync());
+    }
 
     [HttpPost]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
